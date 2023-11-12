b17b765e3789a80b017df4f1648954f90f0af3bf
Micah <micahmartin@gmail.com>
2023-11-11 17:22:37 -0700

    adds start

---
+++ start.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+
+VERSION=20230503
+JAR_FILE=fitnesse-standalone-$VERSION.jar
+
+if ! [ -f $JAR_FILE ]; then
+  echo "Downloading $JAR_FILE"
+  rm fitnesse-standalone.jar
+  wget https://raw.githubusercontent.com/fitnesse/fitnessedotorg/master/releases/$VERSION/fitnesse-standalone.jar
+  mv fitnesse-standalone.jar $JAR_FILE
+fi
+
+java -classpath classes:$JAR_FILE fitnesseMain.FitNesseMain -p 8080
+
