1195f7b7195c71cc6b51ab631fdd3e7ca5d8ea6b
Micah <micahmartin@gmail.com>
2023-11-13 18:13:20 -0700

    using jars int he releases dir

--- start.sh
+++ start.sh
@@ -3,15 +3,5 @@
 VERSION=`cat VERSION`
 echo "Using FitNesse Version $VERSION."
 
-JAR_FILE=fitnesse-standalone-$VERSION.jar
-
-
-if ! [ -f $JAR_FILE ]; then
-  echo "Downloading $JAR_FILE"
-  rm fitnesse-standalone.jar
-  wget https://raw.githubusercontent.com/fitnesse/fitnessedotorg/master/releases/$VERSION/fitnesse-standalone.jar
-  mv fitnesse-standalone.jar $JAR_FILE
-fi
-
-java -classpath classes:$JAR_FILE fitnesseMain.FitNesseMain -p 8080
+java -classpath classes:releases/$VERSION/fitnesse-standalone.jar fitnesseMain.FitNesseMain -p 8080
 
